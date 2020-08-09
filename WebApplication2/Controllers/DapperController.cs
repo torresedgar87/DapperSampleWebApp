84f06cf7f7e177d63f70f52d79677889c3b6ae1e
Edgar Torres Roldan <edgar.torresroldan@petdesk.com>
2020-08-09 15:06:03 -0700

    all files

---
+++ WebApplication2/Controllers/DapperController.cs
@@ -0,0 +1,46 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    [ApiController]
+    public class DapperController : ControllerBase
+    {
+        private string connectionString = "server=localhost\\SQLEXPRESS;database=summerfun;Trusted_Connection=True;";
+
+        [HttpPost]
+        [Route("/Persons")]
+        public async Task<ActionResult> Post()
+        {
+            var rows = 0;
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                db.Open();
+                rows = await db.ExecuteAsync("INSERT INTO dbo.Persons([Name],[CreateDateTime]) VALUES(@Name,@CreateDateTime)", new { Name = "Edgar", CreateDateTime = DateTime.Now });
+            }
+
+            return new OkObjectResult(rows);
+        }
+
+        [HttpGet]
+        [Route("/Persons")]
+        public async Task<ActionResult<object>> Get()
+        {
+            List<Person> persons = null;
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                db.Open();
+                persons = (await db.QueryAsync<Person>("SELECT * FROM dbo.Persons")).ToList();
+            }
+
+            return new OkObjectResult(persons);
+        }
+    }
+}
---
+++ WebApplication2/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string Name { get; set; }
+        public DateTime CreateDateTime { get; set; }
+    }
+}
